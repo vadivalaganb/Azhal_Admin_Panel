52277ea4d1b0bd0c2f9f5bfe3ecf3e32ae60c8b6
Vinothkumar <vinoth3941@gmail.com>
2025-09-23 16:11:09 +0530

    Logo

--- src/app/shared/components/topbar/topbar.component.ts
+++ src/app/shared/components/topbar/topbar.component.ts
@@ -1,10 +1,12 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
   templateUrl: './topbar.component.html',
-  styleUrls: ['./topbar.component.scss']
+  styleUrls: ['./topbar.component.scss'],
+  imports: [RouterLink]
 })
 export class TopbarComponent {
   @Output() toggleSidebarEvent = new EventEmitter<void>();
